d9df3403c9512b803ab5bc0fc7a41aca470a57b9
iamtylerd <tylerdanielphotography@gmail.com>
2022-07-25 17:24:46 -0400

    initial push

--- ashes_forum/ashes_forum/settings.py
+++ ashes_forum/ashes_forum/settings.py
@@ -81,7 +81,15 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    },
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'database-1',
+    #     'USER': config('DB_USER'),
+    #     'PASSWORD': config('DB_PASSWORD'),
+    #     'HOST': config('DB_HOST'),
+    #     'PORT': config('DB_PORT'),
+    # }
 }
 
 
